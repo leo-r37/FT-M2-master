ed23ba357acd037993e678d377f4927d8b4bad51
leo-r37 <leandro_rocha93@hotmail.com>
2022-10-07 20:17:28 -0300

    homework 10 done

--- 10-React-Forms/homework/src/Form.jsx
+++ 10-React-Forms/homework/src/Form.jsx
@@ -1,9 +1,88 @@
 import React from 'react';
+import { useState } from 'react';
+
+
+export function validate(input) {
+  let errors = {};
+  if (!input.username) {
+    errors.username = 'Username is required';
+  } else if (!/\S+@\S+\.\S+/.test(input.username)) {
+    errors.username = 'Username is invalid';
+  } else if (!input.password) {
+    errors.password = 'Password is required';
+  } else if (!/(?=.*[0-9])/.test(input.password)) {
+    errors.password = 'Password is invalid';
+  }
+  return errors;
+}
+
 
 export default function  Form() {
+
+  const [input, setInput] = useState({
+    username: '',
+    password: '',
+  });
+
+  const [valid, setValid] = useState(true);
+
+  const validateForm = (errors) => {
+    let valid = true;
+    Object.values(errors).forEach( (val) => val.length > 0 && (valid = false)
+    );
+    if(valid) {
+      setValid(false)
+    } else {
+      setValid(true)
+    }
+  }
+
+  const [errors, setErrors] = useState({});
+
+  const handleInputChange = (e) => {
+    setErrors(validate({
+      ...input,
+      [e.target.name]: e.target.value
+    }));
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value
+    });
+    validateForm(errors);
+  }
+
   return (
+    <form>
       <div>
-        Componente Form
+        <label>Username:</label>
+        <input
+          className={errors.username && 'danger'}
+          key='username'
+          type='text'
+          name='username'
+          value={input.username}
+          onChange={handleInputChange}
+        />
+        {errors.username && (
+          <span className='danger'>{errors.username}</span>
+        )}
       </div>
+
+      <div>
+        <label>Password:</label>
+        <input
+          className={errors.password && 'danger'}
+          key='password'
+          type='password'
+          name='password'
+          value={input.password}
+          onChange={handleInputChange}
+        />
+        {errors.password && (
+          <span className='danger'>{errors.password}</span>
+        )}
+      </div>
+      <input disabled={valid} type='submit' value='Send' />
+    </form>
   )
 }
